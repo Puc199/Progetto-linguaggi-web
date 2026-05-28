d530bbc441d403fb64d290a0d238609378cdf107
bosskb00 <kristianbof@gmail.com>
2026-05-28 08:46:57 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,16 +1,16 @@
 <?php
 $host = 'localhost';
-$dbname = 'EasyTicket';
+$dbname = 'Prova';
 $user = 'root';
 $pass = 'root';
 $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
 
-try {
-    $pdo = new PDO($dsn, $user, $pass, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+try { 
+    $pdo = new PDO($dsn, $user, $pass, [ // stabiliamo la connessione
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Gestione sicura degli errori: cattura eccezioni PDO e previene terminazioni improvvise.
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC // restituisce risultati delle query
     ]);
-} catch (PDOException $e) {
+} catch (PDOException $e) { //gestione errore 
     die("Connessione fallita: " . $e->getMessage());
 }
 ?>
