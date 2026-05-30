f10d8cdfca35d16355c8e3348435b976cfddd104
Puc199 <pucci.1994172@studenti.uniroma1.it>
2026-05-30 11:46:22 +0200

    database

--- EasyTicket.sql
+++ EasyTicket.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Creato il: Mag 27, 2026 alle 19:48
+-- Creato il: Mag 30, 2026 alle 08:00
 -- Versione del server: 8.0.44
 -- Versione PHP: 8.3.30
 
@@ -44,10 +44,7 @@ CREATE TABLE `biglietto` (
 --
 
 INSERT INTO `biglietto` (`id`, `sigillo_fiscale`, `disponibilita`, `id_utente`, `id_evento_settore`, `posto`, `prezzo`, `data_acquisto`, `stato_rimborso`) VALUES
-(233, '607822CBA2714489', 0, 5, 25627, 1, 24.90, '2026-05-27 18:34:39', 'rimborsato'),
-(234, 'DB214F701B095588', 0, 5, 25627, 2, 24.90, '2026-05-27 18:34:39', 'rimborsato'),
-(235, '4CAAA6FE618DD924', 0, 5, 25628, 1, 34.90, '2026-05-27 20:03:07', 'rimborsato'),
-(236, '814366CF800565E7', 0, 5, 25628, 2, 34.90, '2026-05-27 20:03:07', 'rimborsato');
+(252, 'E060210F3453B57C', 1, 5, 25664, 1, 24.90, '2026-05-29 16:48:42', 'nessuno');
 
 -- --------------------------------------------------------
 
@@ -91,8 +88,11 @@ CREATE TABLE `evento` (
 --
 
 INSERT INTO `evento` (`id`, `titolo`, `descrizione`, `id_categoria`, `id_luogo`, `immagine`, `stato`) VALUES
-(21, 'Roma Live Night', 'Una notte pensata per chi vuole vivere il concerto come un’esperienza completa: luci, palco dinamico, visual immersivi e una scaletta che alterna brani recenti e pezzi storici. L’apertura è affidata a un artista emergente della scena romana, seguito da una band che porta sul palco energia, chitarre e ritmi serrati. Il pubblico viene accompagnato in un viaggio sonoro costruito per far cantare tutta la sala, dal primo all’ultimo minuto.', 1, 1, 'img/eventi/1779800981_1779281753_Xnip2026-05-20_14-55-44.jpg', 'annullato'),
-(22, 'aa', 'aaa', 3, 7, NULL, 'annullato');
+(31, 'Note sotto le stelle', 'Una serata elegante tra musica dal vivo, luci soffuse e arrangiamenti intensi. Un evento pensato per chi vuole lasciarsi coinvolgere da un\'atmosfera raffinata e da un sound capace di emozionare fin dalle prime note.', 1, 1, 'img/eventi/1780049717_1780049539.png', 'programmato'),
+(32, 'Ritmi di città', 'Un live energico e contemporaneo che porta sul palco sonorità moderne, presenza scenica e un coinvolgimento costante del pubblico. Ideale per chi cerca una serata dinamica, urbana e piena di ritmo.', 1, 2, 'img/eventi/1780049981_1779278225_Xnip2026-05-20_13-53-19.jpg', 'programmato'),
+(33, 'Voci di scena', 'Uno spettacolo teatrale intenso e coinvolgente, costruito su dialoghi incisivi, interpretazioni curate e una messa in scena capace di alternare emozione, tensione e profondità narrativa.', 2, 4, 'img/eventi/1780050000_1779718179_1779281699_Xnip2026-05-20_14-54-24.jpg', 'programmato'),
+(34, 'L\'arte della commedia', 'Una commedia brillante dal ritmo vivace, con personaggi ben caratterizzati, situazioni imprevedibili e momenti leggeri che accompagnano lo spettatore in una serata piacevole e divertente.', 2, 5, 'img/eventi/1780049882_1780049856df9b.png', 'programmato'),
+(35, 'Estate in festa', 'Un festival all\'aperto che unisce musica, intrattenimento e atmosfera conviviale in un contesto ricco di energia. Perfetto per vivere una giornata speciale tra spettacolo, socialità e voglia di stare insieme.', 3, 7, 'img/eventi/1780049965_1779281317_Xnip2026-05-20_13-56-39.jpg', 'programmato');
 
 -- --------------------------------------------------------
 
@@ -115,9 +115,54 @@ CREATE TABLE `evento_settore` (
 --
 
 INSERT INTO `evento_settore` (`id`, `id_replica_evento`, `id_evento`, `id_settore`, `prezzo`, `posti_totali`, `posti_disponibili`) VALUES
-(25627, 4536, 21, 3, 24.90, 80, 78),
-(25628, 4536, 21, 2, 34.90, 150, 148),
-(25629, 4536, 21, 1, 49.90, 120, 120);
+(25654, 4554, 35, 24, 35.00, 400, 400),
+(25655, 4554, 35, 23, 59.90, 230, 230),
+(25656, 4554, 35, 25, 22.00, 150, 150),
+(25657, 4554, 35, 22, 89.90, 120, 120),
+(25658, 4555, 35, 24, 35.00, 400, 400),
+(25659, 4555, 35, 23, 59.90, 230, 230),
+(25660, 4555, 35, 25, 22.00, 150, 150),
+(25661, 4555, 35, 22, 89.90, 120, 120),
+(25662, 4543, 31, 1, 49.90, 120, 120),
+(25663, 4543, 31, 2, 34.90, 150, 150),
+(25664, 4543, 31, 3, 24.90, 80, 79),
+(25665, 4544, 31, 1, 49.90, 120, 120),
+(25666, 4544, 31, 2, 34.90, 150, 150),
+(25667, 4544, 31, 3, 24.90, 80, 80),
+(25668, 4545, 32, 4, 79.90, 180, 180),
+(25669, 4545, 32, 5, 49.90, 370, 370),
+(25670, 4545, 32, 6, 34.90, 250, 250),
+(25671, 4545, 32, 7, 24.90, 100, 100),
+(25672, 4546, 32, 4, 79.90, 180, 180),
+(25673, 4546, 32, 5, 49.90, 370, 370),
+(25674, 4546, 32, 6, 34.90, 250, 250),
+(25675, 4546, 32, 7, 24.90, 100, 100),
+(25676, 4547, 32, 4, 79.90, 180, 180),
+(25677, 4547, 32, 5, 49.90, 370, 370),
+(25678, 4547, 32, 6, 34.90, 250, 250),
+(25679, 4547, 32, 7, 24.90, 100, 100),
+(25680, 4548, 33, 12, 95.00, 320, 320),
+(25681, 4548, 33, 13, 120.00, 180, 180),
+(25682, 4548, 33, 14, 85.00, 150, 150),
+(25683, 4548, 33, 15, 55.00, 300, 300),
+(25684, 4549, 34, 16, 70.00, 260, 260),
+(25685, 4549, 34, 17, 85.00, 140, 140),
+(25686, 4549, 34, 18, 45.00, 300, 300),
+(25687, 4550, 34, 16, 70.00, 260, 260),
+(25688, 4550, 34, 17, 85.00, 140, 140),
+(25689, 4550, 34, 18, 45.00, 300, 300),
+(25690, 4551, 35, 22, 89.90, 120, 120),
+(25691, 4551, 35, 23, 59.90, 230, 230),
+(25692, 4551, 35, 24, 35.00, 400, 400),
+(25693, 4551, 35, 25, 22.00, 150, 150),
+(25694, 4552, 35, 22, 89.90, 120, 120),
+(25695, 4552, 35, 23, 59.90, 230, 230),
+(25696, 4552, 35, 24, 35.00, 400, 400),
+(25697, 4552, 35, 25, 22.00, 150, 150),
+(25698, 4553, 35, 22, 89.90, 120, 120),
+(25699, 4553, 35, 23, 59.90, 230, 230),
+(25700, 4553, 35, 24, 35.00, 400, 400),
+(25701, 4553, 35, 25, 22.00, 150, 150);
 
 -- --------------------------------------------------------
 
@@ -200,7 +245,18 @@ INSERT INTO `notifica` (`id`, `id_utente`, `titolo`, `messaggio`, `data_creazion
 (29, 5, 'Rimborso automatico effettuato', 'L\'evento annullato ha generato un rimborso automatico di € 24,90 sul tuo wallet.', '2026-05-27 19:00:29', 0),
 (30, 5, 'Rimborso automatico effettuato', 'L\'evento annullato ha generato un rimborso automatico di € 24,90 sul tuo wallet.', '2026-05-27 19:00:29', 0),
 (31, 5, 'Rimborso automatico effettuato', 'L\'evento annullato ha generato un rimborso automatico di € 34,90 sul tuo wallet.', '2026-05-27 20:03:42', 0),
-(32, 5, 'Rimborso automatico effettuato', 'L\'evento annullato ha generato un rimborso automatico di € 34,90 sul tuo wallet.', '2026-05-27 20:03:42', 0);
+(32, 5, 'Rimborso automatico effettuato', 'L\'evento annullato ha generato un rimborso automatico di € 34,90 sul tuo wallet.', '2026-05-27 20:03:42', 0),
+(33, 5, 'Rimborso automatico effettuato', 'La replica annullata ha generato un rimborso automatico di € 19,90 sul tuo wallet.', '2026-05-28 22:04:54', 0),
+(34, 5, 'Rimborso automatico effettuato', 'La replica annullata ha generato un rimborso automatico di € 19,90 sul tuo wallet.', '2026-05-28 22:10:57', 0),
+(35, 5, 'Rimborso automatico effettuato', 'La replica annullata ha generato un rimborso automatico di € 19,90 sul tuo wallet.', '2026-05-28 22:10:57', 0),
+(36, 5, 'Rimborso automatico effettuato', 'La replica annullata ha generato un rimborso automatico di € 19,90 sul tuo wallet.', '2026-05-28 22:18:29', 0),
+(37, 5, 'Rimborso automatico effettuato', 'La replica annullata ha generato un rimborso automatico di € 19,90 sul tuo wallet.', '2026-05-28 22:18:29', 0),
+(38, 5, 'Rimborso automatico effettuato', 'La replica annullata ha generato un rimborso automatico di € 19,90 sul tuo wallet.', '2026-05-28 22:18:29', 0),
+(39, 5, 'Rimborso automatico effettuato', 'La replica annullata ha generato un rimborso automatico di € 19,90 sul tuo wallet.', '2026-05-28 22:29:03', 0),
+(40, 5, 'Rimborso automatico effettuato', 'La replica annullata ha generato un rimborso automatico di € 19,90 sul tuo wallet.', '2026-05-28 22:29:03', 0),
+(41, 5, 'Rimborso automatico effettuato', 'La replica annullata ha generato un rimborso automatico di € 19,90 sul tuo wallet.', '2026-05-28 22:29:03', 0),
+(42, 5, 'Rimborso automatico effettuato', 'La replica annullata ha generato un rimborso automatico di € 19,90 sul tuo wallet.', '2026-05-28 22:33:53', 0),
+(43, 5, 'Rimborso automatico effettuato', 'La replica annullata ha generato un rimborso automatico di € 19,90 sul tuo wallet.', '2026-05-28 22:33:53', 0);
 
 -- --------------------------------------------------------
 
@@ -221,7 +277,19 @@ CREATE TABLE `replica_evento` (
 --
 
 INSERT INTO `replica_evento` (`id`, `id_evento`, `data_ora_inizio`, `data_ora_fine`, `stato`) VALUES
-(4536, 21, '2026-07-02 03:00:00', '2026-07-02 03:00:00', 'annullata');
+(4543, 31, '2026-06-14 21:00:00', NULL, 'programmata'),
+(4544, 31, '2026-06-15 21:00:00', NULL, 'programmata'),
+(4545, 32, '2026-06-20 21:30:00', NULL, 'programmata'),
+(4546, 32, '2026-06-21 21:30:00', NULL, 'programmata'),
+(4547, 32, '2026-06-22 21:30:00', NULL, 'programmata'),
+(4548, 33, '2026-06-18 20:30:00', NULL, 'programmata'),
+(4549, 34, '2026-06-25 20:45:00', NULL, 'programmata'),
+(4550, 34, '2026-06-26 20:45:00', NULL, 'programmata'),
+(4551, 35, '2026-07-03 18:00:00', NULL, 'programmata'),
+(4552, 35, '2026-07-04 18:00:00', NULL, 'programmata'),
+(4553, 35, '2026-07-05 18:00:00', NULL, 'programmata'),
+(4554, 35, '2026-07-06 18:00:00', NULL, 'programmata'),
+(4555, 35, '2026-07-07 18:00:00', NULL, 'programmata');
 
 -- --------------------------------------------------------
 
@@ -340,7 +408,7 @@ INSERT INTO `utente` (`id`, `nome`, `cognome`, `data_nascita`, `username`, `pass
 (2, 'Riccardo', 'Pucci', '2002-09-01', 'Pucc199', '$2y$10$JwPad7ScwK4Q1ukO9usftuhda67xZa5d4mUL5q4uUHfV0V/ffCHbG', 2, 125.00),
 (3, 'Riccardo', 'Pucci', '2002-09-01', 'lalala', '$2y$10$/YWWwtkfv6gy0Fy8C2sSkOYJGKdqXarwmtnLJBHwFxzquPf4y/3lW', 2, 7830.00),
 (4, 'sa', 'sasa', '2002-09-01', 'sasasa', '$2y$10$OUyGFOTXLVlaYVCJ4h0.8eewSATBXN655zlMlt95Y/GDZ4cQie9Le', 2, 10245.20),
-(5, 'Riccardo', 'Pucci', '2002-09-01', 'Pucc1994172', '$2y$10$WLQWxPiBcWTY7OfSH1TsS.C3Aro61dKLb9t93zIWZ0MQWEZcFKYqm', 2, 2000.20),
+(5, 'Riccardo', 'Pucci', '2002-09-01', 'Pucc1994172', '$2y$10$WLQWxPiBcWTY7OfSH1TsS.C3Aro61dKLb9t93zIWZ0MQWEZcFKYqm', 2, 2015.30),
 (6, '\'\';DELETE * FROM utente', 'A', '1911-01-01', 'cancellino', '$2y$10$FFXBrGT1UTmpGYYBzHcJ7uUHBdhZGWPvfR6gV2OT48qbzJvMAfOvy', 2, 3000.00);
 
 --
@@ -440,7 +508,7 @@ ALTER TABLE `utente`
 -- AUTO_INCREMENT per la tabella `biglietto`
 --
 ALTER TABLE `biglietto`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=238;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=254;
 
 --
 -- AUTO_INCREMENT per la tabella `categoria`
@@ -452,13 +520,13 @@ ALTER TABLE `categoria`
 -- AUTO_INCREMENT per la tabella `evento`
 --
 ALTER TABLE `evento`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- AUTO_INCREMENT per la tabella `evento_settore`
 --
 ALTER TABLE `evento_settore`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25630;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25718;
 
 --
 -- AUTO_INCREMENT per la tabella `luogo`
@@ -470,19 +538,19 @@ ALTER TABLE `luogo`
 -- AUTO_INCREMENT per la tabella `notifica`
 --
 ALTER TABLE `notifica`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT per la tabella `replica_evento`
 --
 ALTER TABLE `replica_evento`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4537;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4560;
 
 --
 -- AUTO_INCREMENT per la tabella `rimborsi`
 --
 ALTER TABLE `rimborsi`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- AUTO_INCREMENT per la tabella `ruolo`
